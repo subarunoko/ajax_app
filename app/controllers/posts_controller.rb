abdc4c165c3938e207d991ec3952a64190851ab1
subarunoko <kazenonakasubaru@yahoo.co.jp>
2021-01-09 22:45:23 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,20 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  # def new
+  # end
 
   
   def create
-    #Post.create(content: params[:content])
-
+    Post.create(content: params[:content])
+    redirect_to action :index
+    
     #下の表記でも可
-    post = Post.new(content: params[:content])
-    post.save
+    # post = Post.new(content: params[:content])
+    # post.save
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "posts", to: "posts#index"
-  get "posts/new", to: "posts#new"
+  # get "posts", to: "posts#index"
+  # get "posts/new", to: "posts#new"
+  root to: "posts#index"
   post "posts", to: "posts#create"
 end
